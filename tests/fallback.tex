latex3/luaotfload

commit b9d377647240804d7d85429a18e1bdf0ff00823c

Remove spurious else
This was a leftover from old code. This should fix Adobe Garamond Pro
issue.

otfl-font-nms.lua
@@ -190,8 +190,6 @@ function names.resolve(specification)
                             found[1] = face
                             break
                         end
-                    else
-                        found[1] = face
                     end
                 else
                     if name == fullname

commit 54d2c5f1f4e5aaccadf4f4af472a497cab9645c8

Fallback to regular
If the requested style is not found, fallback to regular. I think this
broke in commit b9d377647240804d7d85429a18e1bdf0ff00823c

closes #13

otfl-font-nms.lua
@@ -189,6 +189,9 @@ function names.resolve(specification)
                             found[1] = face
                             break
                         end
+                    elseif subfamily == "regular"
+                        or synonyms.regular[style] then
+                        found.fallback = face
                     end
                 else
                     if name == fullname
@@ -237,6 +240,8 @@ function names.resolve(specification)
                                 name, style, closest.filename[1])
                     return closest.filename[1], closest.filename[2]
                 end
+            elseif found.fallback then
+                return found.fallback.filename[1], found.fallback.filename[2]
             end
             -- no font found so far
             if not reloaded then

tests/fallback.tex
@@ -1,0 +1,6 @@
+\input luaotfload.sty
+\font\testa={XITS Math}
+\font\testb={XITS Math/B}
+\testa text\par
+\testb text\par
+\bye
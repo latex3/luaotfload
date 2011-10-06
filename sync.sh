e201b64c363a98b9e2ade88ac4140b49e10a87b6
Khaled Hosny <khaledhosny@eglug.org>
2011-10-06 02:16:49 +0200

    Of no much use now

--- sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CTX_TEXFM=$1
-CTX_FILES=`grep -o "loadmodule('....-....lua')" $CTX_TEXFM/tex/generic/context/luatex-fonts.lua |\
-           sed -e "s/loadmodule('\(....-....lua\)')/\1/" | sort | uniq`
-
-echo $CTX_FILES
-for i in $CTX_FILES; do
-	cp -v $CTX_TEXFM/tex/context/base/$i otfl-$i
-done

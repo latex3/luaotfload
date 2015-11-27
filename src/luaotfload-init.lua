36c87427e9bba1b3ca1dc58a0d6f914ca21bc9a6
Philipp Gesang <phg@phi-gamma.net>
2015-11-28 00:08:20 +0100

    [doc,init] clarify default/reference distinction
    * default:   our package (without lualibs)
    * reference: the package from upstream

--- src/luaotfload-init.lua
+++ src/luaotfload-init.lua
@@ -292,8 +292,17 @@ local init_main = function ()
                                         or "reference"
   fontloader = tostring (fontloader)
 
-  if fontloader == "reference" or fontloader == "default" then
+  if fontloader == "reference" then
     logreport ("log", 4, "init", "Using reference fontloader.")
+    load_fontloader_module (luaotfload.fontloader_package)
+
+  elseif fontloader == "default" then
+    --- Same as above but loader name not correctly replaced by the file name
+    --- of our fontloader package. Perhaps something’s wrong with the status
+    --- file which contains the datestamped filename? In any case, it can’t
+    --- hurt reporting it as a bug.
+    logreport ("both", 0, "init", "Fontloader substitution failed, got “default”.")
+    logreport ("log",  4, "init", "Falling back to reference fontloader.")
     load_fontloader_module (luaotfload.fontloader_package)
 
   elseif fontloader == "unpackaged" then

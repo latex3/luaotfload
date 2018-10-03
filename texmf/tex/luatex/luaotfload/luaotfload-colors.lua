d57639691665dc3e8e744ccfc4a8be94fc535e37
Ulrike Fischer <fischer@troubleshooting-tex.de>
2018-10-03 12:16:19 +0200

    install files with correct version in texmf

--- texmf/tex/luatex/luaotfload/luaotfload-colors.lua
+++ texmf/tex/luatex/luaotfload/luaotfload-colors.lua
@@ -1,5 +1,5 @@
 if not modules then modules = { } end modules ['luaotfload-colors'] = {
-    version   = "2.9",
+    version   = "2.91",
     comment   = "companion to luaotfload-main.lua (font color)",
     author    = "Khaled Hosny, Elie Roux, Philipp Gesang, Dohyun Kim, David Carlisle",
     copyright = "Luaotfload Development Team",

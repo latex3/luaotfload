7627d225b99b24a63bacdabde492a22a49158ebb
Khaled Hosny <khaledhosny@eglug.org>
2018-10-26 23:05:18 +0200

    Handle dir nodes

--- src/harf-node.lua
+++ src/harf-node.lua
@@ -3,11 +3,14 @@ local hb = require("harf-base")
 local disccode  = node.id("disc")
 local gluecode  = node.id("glue")
 local glyphcode = node.id("glyph")
+local dircode   = node.id("dir")
+local parcode   = node.id("local_par")
 local spaceskip = 13
 
-local function shape(head, current, run, nodes, codes, direction)
+local function shape(head, current, run, nodes, codes)
   local offset = run.start
   local len = run.len
+  local dir = run.dir
   local fontid = run.font
   local fontdata = fontid and font.fonts[fontid]
 
@@ -23,6 +26,9 @@ local function shape(head, current, run, nodes, codes, direction)
 
     buf:add_codepoints(codes, offset - 1, len)
     if hb.shape(fontdata.hb.font, buf, options) then
+      if dir == "TRT" then
+        buf:reverse()
+      end
       local glyphs = buf:get_glyph_infos_and_positions()
       for _, g in next, glyphs do
         -- Copy the node for the first character in the cluster, so that we
@@ -75,6 +81,7 @@ local function process(head, groupcode, size, packtype, direction)
     return head
   end
 
+  local dirstack = {}
   local dir = direction or "TLT"
   local nodes, codes = {}, {}
   local runs = { { font = fontid, dir = dir, start = 1, len = 0 } }
@@ -82,20 +89,34 @@ local function process(head, groupcode, size, packtype, direction)
   for n in node.traverse(head) do
     local id = n.id
     local char = 0xFFFC -- OBJECT REPLACEMENT CHARACTER
+    local currdir = dir
+    local currfont = fontid
 
     if id == glyphcode then
-      if n.font ~= fontid then
-        runs[#runs + 1] = { font = n.font, dir = dir, start = i, len = 0 }
-      end
-      fontid = n.font
+      currfont = n.font
       char = n.char
     elseif id == gluecode and n.subtype == spaceskip then
       char = 0x0020 -- SPACE
     elseif id == disccode then
       -- XXX actually handle this
       char = 0x00AD -- SOFT HYPHEN
+    elseif id == dircode then
+      if n.dir:sub(1, 1) == "+" then
+        table.insert(dirstack, currdir)  -- push
+        currdir = n.dir:sub(2)
+      else
+        currdir = table.remove(dirstack) -- pop
+      end
+    elseif id == parcode then
+      currdir = n.dir
     end
 
+    if currfont ~= fontid or currdir ~= dir then
+      runs[#runs + 1] = { font = currfont, dir = currdir, start = i, len = 0 }
+    end
+
+    fontid = currfont
+    dir = currdir
     runs[#runs].len = runs[#runs].len + 1
 
     nodes[#nodes + 1] = n

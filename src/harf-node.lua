381a92c9d4710f056631739bb89dac5bfa2f3683
Khaled Hosny <khaledhosny@eglug.org>
2018-10-29 20:45:33 +0200

    Make text extraction from PDF work
    Fixes https://github.com/khaledhosny/luahbtex-harf/issues/1

--- src/harf-node.lua
+++ src/harf-node.lua
@@ -6,6 +6,50 @@ local glyphcode = node.id("glyph")
 local dircode   = node.id("dir")
 local parcode   = node.id("local_par")
 local spaceskip = 13
+local directmode = 2
+
+-- Convert integer to UTF-16 hex string used in PDF.
+local function to_utf16_hex(uni)
+  if uni < 0x10000 then
+    return string.format("%04X", uni)
+  else
+    uni = uni - 0x10000
+    local hi = 0xD800 + bit32.rshift(uni, 10) -- 0xD800 + (uni // 0x400)
+    local lo = 0xDC00 + (uni % 0x400)
+    return string.format("%04X%04X", hi, lo)
+  end
+end
+
+-- Find how many characters are part of this glyph.
+--
+-- The first return value is the number of characters, with the these special
+-- values:
+--   0 means it is inside a multi-glyph cluster
+--  -1 means as many characters tell the end of the run
+--
+-- The second return value is the number of glyph in this cluster.
+--
+local function chars_in_glyph(i, glyphs)
+  local nchars, nglyphs = 0, 0
+  local cluster = glyphs[i].cluster
+
+  -- Glyph is not the first in cluster
+  if glyphs[i - 1] and glyphs[i - 1].cluster == cluster then
+    return 0, 0
+  end
+
+  while glyphs[i + nglyphs] and glyphs[i + nglyphs].cluster == cluster do
+    nglyphs = nglyphs + 1
+  end
+
+  if glyphs[i + nglyphs] then
+    nchars = glyphs[i + nglyphs].cluster - cluster
+  else
+    nchars = -1
+  end
+
+  return nchars, nglyphs
+end
 
 local function shape(head, current, run, nodes, codes)
   local offset = run.start
@@ -39,19 +83,21 @@ local function shape(head, current, run, nodes, codes)
 
       local characters = {} -- LuaTeX font characters table
       local glyphs = buf:get_glyph_infos_and_positions()
-      for _, g in next, glyphs do
+      for i, g in next, glyphs do
         -- Copy the node for the first character in the cluster, so that we
         -- inherit any of its properties.
-        local n = node.copy(nodes[g.cluster + 1])
-        local id = n.id
         local gid = g.codepoint
+        local char = hb.CH_GID_PREFIX + gid
+        local index = g.cluster + 1
+        local n = node.copy(nodes[index])
+        local id = n.id
 
         head, current = node.insert_after(head, current, n)
 
         if id == glyphcode then
           local width = hbfont:get_glyph_h_advance(gid)
 
-          n.char = hb.CH_GID_PREFIX + gid
+          n.char = char
           n.xoffset = rtl and -g.x_offset or g.x_offset
           n.yoffset = g.y_offset
 
@@ -72,13 +118,54 @@ local function shape(head, current, run, nodes, codes)
           -- Load the glyph metrics of not already loaded.
           if not loaded[gid] then
             local extents = hbfont:get_glyph_extents(gid)
-            characters[hb.CH_GID_PREFIX + gid] = {
+            local character = {
               index = gid,
               width = width,
               height = extents and extents.y_bearing or ascender,
               depth = -(extents and extents.y_bearing + extents.height or descender),
+              tounicode = tounicode
             }
-            loaded[gid] = true
+            loaded[gid] = character
+            characters[char] = character
+          end
+
+          -- Handle PDF text extraction:
+          -- * Find how many characters in this cluster and how many glyphs,
+          -- * If there is more than 0 characters
+          --   * One glyph: one to one or one to many mapping, can be
+          --     represented by font’s /ToUnicode
+          --   * More than one: many to one or many to many mapping, can be
+          --     represented by /ActualText spans.
+          -- * If there are zero characters, then this glyph is part of complex
+          --   cluster that will be covered by an /ActualText span.
+          local nchars, nglyphs = chars_in_glyph(i, glyphs)
+          nchars = nchars >= 0 and nchars or offset + len - index
+          if nchars > 0 then
+            local tounicode = ""
+            for j = 0, nchars - 1 do
+              local id = nodes[index + j].id
+              if id == glyphcode or id == gluecode then
+                tounicode = tounicode..to_utf16_hex(codes[index + j])
+              end
+            end
+            if tounicode ~= "" then
+              if nglyphs == 1 and not loaded[gid].tounicode then
+                loaded[gid].tounicode = tounicode
+                characters[char] = loaded[gid]
+              elseif tounicode ~= loaded[gid].tounicode then
+                local actual = node.new("whatsit", "pdf_literal")
+                actual.mode = directmode
+                actual.data = "/Span<</ActualText<FEFF"..tounicode..">>>BDC"
+                head = node.insert_before(head, current, actual)
+                glyphs[i + nglyphs - 1].endactual = true
+              end
+            end
+          end
+          if g.endactual then
+            local actual = node.new("whatsit", "pdf_literal")
+            actual.mode = directmode
+            actual.data = "EMC"
+            head, current = node.insert_after(head, current, actual)
           end
         elseif id == gluecode and n.subtype == spaceskip then
           if n.width ~= g.x_advance then

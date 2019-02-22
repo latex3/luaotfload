dadf253190a334a6bd78334909bc131a690e2e07
Ulrike Fischer <fischer@troubleshooting-tex.de>
2019-02-22 16:46:19 +0100

    install iwona for tests

--- texlive.sh
+++ texlive.sh
@@ -42,7 +42,7 @@ tlmgr install fontspec microtype unicode-math luaotfload
 tlmgr install  sourcecodepro Asana-Math  ebgaramond  tex-gyre  amsfonts gnu-freefont  
 tlmgr install  opensans fira tex-gyre-math junicode lm  lm-math amiri ipaex xits
 tlmgr install  libertine coelacanth fontawesome stix2-otf
-tlmgr install  luatexko unfonts-core cjk-ko
+tlmgr install  luatexko unfonts-core cjk-ko iwona
 
 # languages
 tlmgr install  luatexja arabluatex babel babel-english

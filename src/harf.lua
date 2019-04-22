313f7297ed4e70ae8659a97d6d9f599fc895d364
Khaled Hosny <khaledhosny@eglug.org>
2019-04-22 21:21:42 +0200

    Use pre_output_filter for post processing nodes
    It is both more accurate and slightly faster than post_linebreak_filter.

--- src/harf.lua
+++ src/harf.lua
@@ -7,7 +7,7 @@ harf.callbacks = {
   define_font = define_font,
   pre_linebreak_filter = harf_node.process,
   hpack_filter = harf_node.process,
-  post_linebreak_filter = harf_node.post_process,
+  pre_output_filter = harf_node.post_process,
   wrapup_run = harf_node.cleanup,
   get_char_tounicode = harf_node.get_tounicode
 }

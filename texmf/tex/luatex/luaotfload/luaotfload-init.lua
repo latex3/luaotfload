4fd930efd820da31d2efdad38bb00787fee72507
Marcel Fabian Krüger <tex@2krueger.de>
2019-08-13 13:39:21 +0200

    Fixes, less debugging, ...

--- texmf/tex/luatex/luaotfload/luaotfload-init.lua
+++ texmf/tex/luatex/luaotfload/luaotfload-init.lua
@@ -649,7 +649,7 @@ local init_post_install_callbacks = function ()
   local handler = nodes.simple_font_handler
   local callback = function(head, groupcode, _, _, direction)
     if not direction then
-      direction = tex.get'textdir'
+      direction = head.dir or tex.textdir
     end
     head = dobidi(head, nil, nil, nil, direction)
     return handler(head, groupcode, nil, nil, direction)

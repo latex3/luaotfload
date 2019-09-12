80c31230c1bdc113ea47f86e65a3e289beeceb06
Marcel Fabian Krüger <tex@2krueger.de>
2019-09-13 00:11:46 +0200

    Use x-height defined by font (again)
    Fixes a bug introduced in 0ce5fe0057d9

--- texmf/tex/latex-dev/luaotfload/luaotfload-auxiliary.lua
+++ texmf/tex/latex-dev/luaotfload/luaotfload-auxiliary.lua
@@ -285,7 +285,7 @@ luaotfload_callbacks [#luaotfload_callbacks + 1] = {
 local function set_xheight(tfmdata)
   local parameters = tfmdata.parameters
   if not parameters then return end
-  if not (parameters.x_height or parameters[5] or 0) == 0 then return end
+  if not ((parameters.x_height or parameters[5] or 0) == 0) then return end
   if tfmdata.characters and tfmdata.characters[120] then
     parameters.x_height = tfmdata.characters[120].height
   else

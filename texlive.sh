89bcabfaa55f8d0c3f5aa4169860ae131e7cd222
Marcel Fabian Krüger <tex@2krueger.de>
2019-11-06 13:33:33 +0100

    Combine calls to tlmgr

--- texlive.sh
+++ texlive.sh
@@ -18,58 +18,61 @@ if ! command -v texlua > /dev/null; then
 
   cd ..
 fi
-tlmgr update --self 
+tlmgr update --self
+tlmgr pinning add tlcontrib 'luahbtex*'
+
 if ! tlmgr repository list | grep -q tlcontrib; then
   # Add TL contrib
   tlmgr repository add http://contrib.texlive.info/current tlcontrib
 fi
 
+(
 # Needed for any use of texlua even if not testing LuaTeX
-tlmgr install l3build latex latex-bin  luatex  latex-bin-dev
+echo l3build latex latex-bin  luatex  latex-bin-dev
 
-tlmgr pinning add tlcontrib 'luahbtex*'
-tlmgr install luahbtex
+echo luahbtex
 
 # Required to build plain and LaTeX formats:
 # TeX90 plain for unpacking, pdfLaTeX, LuaLaTeX and XeTeX for tests
-tlmgr install cm etex knuth-lib tex tex-ini-files unicode-data 
+echo cm etex knuth-lib tex tex-ini-files unicode-data
 
 # various tools / dependencies of other packages
-tlmgr install ctablestack filehook ifoddpage iftex luatexbase trimspaces
-tlmgr install oberdiek etoolbox xkeyval ucharcat xstring everyhook
-tlmgr install svn-prov setspace
+echo ctablestack filehook ifoddpage iftex luatexbase trimspaces
+echo oberdiek etoolbox xkeyval ucharcat xstring everyhook
+echo svn-prov setspace
 
 # graphics
-tlmgr install graphics xcolor graphics-def pgf
+echo graphics xcolor graphics-def pgf
 
 # fonts support - perhaps take here luaotfload out of the list ...
 # or is it installed as dependency anyway?
-tlmgr install fontspec microtype unicode-math luaotfload
+echo fontspec microtype unicode-math luaotfload
 
 # fonts
-tlmgr install  sourcecodepro Asana-Math  ebgaramond  tex-gyre  amsfonts gnu-freefont  
-tlmgr install  opensans fira tex-gyre-math junicode lm  lm-math amiri ipaex xits
-tlmgr install  libertine coelacanth fontawesome stix2-otf dejavu
-tlmgr install  luatexko unfonts-core cjk-ko iwona libertinus-fonts fandol
-tlmgr install  cm-unicode
+echo sourcecodepro Asana-Math  ebgaramond  tex-gyre  amsfonts gnu-freefont
+echo opensans fira tex-gyre-math junicode lm  lm-math amiri ipaex xits
+echo libertine coelacanth fontawesome stix2-otf dejavu
+echo luatexko unfonts-core cjk-ko iwona libertinus-fonts fandol
+echo cm-unicode
 
 # languages
-tlmgr install  luatexja arabluatex babel babel-english
-          
+echo luatexja arabluatex babel babel-english
+
 
 # math
-tlmgr install  amsmath lualatex-math  
+echo amsmath lualatex-math
 
 # a few more packages
-tlmgr install  luacode environ adjustbox collectbox ms varwidth geometry url ulem
+echo luacode environ adjustbox collectbox ms varwidth geometry url ulem
 
 # some packages for the documentation
-tlmgr install  standalone luatex85 tikzmarmots tikzducks pgf-blur inconsolata tools caption hyperref metalogo fancyvrb mdwtools titlesec tocloft pdfpages listings
+echo standalone luatex85 tikzmarmots tikzducks pgf-blur inconsolata tools caption hyperref metalogo fancyvrb mdwtools titlesec tocloft pdfpages listings
 
- 
+
 # Assuming a 'basic' font set up, metafont is required to avoid
 # warnings with some packages and errors with others
-tlmgr install metafont mfware
+echo metafont mfware
+) | xargs tlmgr install
 
 # Keep no backups (not required, simply makes cache bigger)
 tlmgr option -- autobackup 0

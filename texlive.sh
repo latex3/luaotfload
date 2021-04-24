801f59c57c8f4cd6212735bf003efd54db433c67
Marcel Fabian Krüger <tex@2krueger.de>
2021-04-24 12:25:31 +0200

    Revert "Use pretest"
    This reverts commit 9012211ef7f826d7abaf4590a8bcf6f8fff0b59f.

--- texlive.sh
+++ texlive.sh
@@ -9,12 +9,12 @@
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
 if ! command -v texlua > /dev/null; then
   # Obtain TeX Live
-  wget http://pretest.math.hamburg/install-tl-unx.tar.gz
+  wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
   tar -xzf install-tl-unx.tar.gz
   cd install-tl-20*
 
   # Install a minimal system
-  ./install-tl --profile=../texlive.profile -repository https://pretest.math.hamburg
+  ./install-tl --profile=../texlive.profile
 
   cd ..
 fi
